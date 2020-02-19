153fdff284efdee177985d775bae3f28e215962f
Matheus H. de Souza <mh.matheussouza@gmail.com>
2020-02-19 14:41:14 -0300

    Adicionando integração com MySQL

--- app/models/ClienteModel.js
+++ app/models/ClienteModel.js
@@ -1,5 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    port: '3307',
+    user: 'root',
+    password: 'mysql2020',
+    database: 'curso_node'
+});
+
 module.exports = () => {
     this.all = () => {
+        con.query('SELECT * FROM clientes', (err, result) => {
+            console.log(result);
+        });
+
         return [{nome: "Teste", email: "mail@mail.com"}];
     };
 
