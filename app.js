49df1d09d3fc5d24c0bb574f3c1cfaf19cc4d47b
Matheus H. de Souza <mh.matheussouza@gmail.com>
2020-02-19 11:04:53 -0300

    Criando views com EJS

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send(`
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                    <title>Introdução a NodeJS</title>
-                </head>
-                <body>
-                    <h1>Página Home</h1>
-                </body>
-            </html>
-        `);
+    res.render('site/home');
 });
 
 app.get('/contato', (req, res) => {
-    res.send(`
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                    <title>Introdução a NodeJS</title>
-                </head>
-                <body>
-                    <h1>Página Contato</h1>
-                </body>
-            </html>
-        `);
+    res.render('site/contato');
 });
 
 app.listen(8000, () => {
